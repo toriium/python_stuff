48e9e0b3ed0d9bce947c5f07a53b45806d58569e
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-06-16 20:01:29 -0300

    feat: decouple

---
+++ envs/_decouple.py
@@ -0,0 +1,16 @@
+"""
+In this case the envs need be in the system.
+ I suggest to use pycharm plugin "EnvFile", it runs your code passing the value from specified .env
+"""
+
+from decouple import config
+
+DB_HOST = config("DB_HOST")
+DB_HOST_DEFAULT = config("DB_HOST_WRONG", default='mysql_server_default')
+VAR_BOOL = config("ENABLE", cast=bool)
+VAR_ALL = config("ALL", default='1', cast=int)
+
+print(DB_HOST)
+print(DB_HOST_DEFAULT)
+print(VAR_BOOL)
+print(VAR_ALL)
