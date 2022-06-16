7b01087d941e48ab647173005f8241b3b8443ff3
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2022-06-16 20:23:44 -0300

    feat: _dotenv_and_pydantic.py

---
+++ envs/_dotenv_and_pydantic.py
@@ -0,0 +1,41 @@
+import os
+
+from dotenv import dotenv_values
+from pydantic import BaseModel
+
+
+def path_to_file(file):
+    env_path = os.path.dirname(os.path.realpath(__file__))
+    path = None
+    for root, dirs, files in os.walk(env_path):
+        for name in files:
+            if name == file:
+                path = (os.path.abspath(os.path.join(root, name)))
+
+    return path
+
+
+def get_env_values() -> dict:
+    default_env = dotenv_values(path_to_file('env.env'))
+    env_type = default_env['ENV']
+
+    if env_type == 'PROD':
+        print('============ ENV PROD ===========')
+        env_values = dotenv_values(path_to_file('.env.prod'))
+    elif env_type == 'HOMOLOG':
+        print('============ ENV HOMOLOG ===========')
+        env_values = dotenv_values(path_to_file('.env.homolog'))
+    elif env_type == 'DEV':
+        print('============ ENV DEV ===========')
+        env_values = dotenv_values(path_to_file('.env.dev'))
+    return env_values
+
+
+class EnvModel(BaseModel):
+    DB_HOST: str
+    DB_USER: str
+
+
+__env_values = get_env_values()
+ENV = EnvModel(**__env_values)
+print(ENV.DB_HOST)
