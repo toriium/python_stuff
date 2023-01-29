64c91f720466249763ea6f88accd05131a4d2345
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2023-01-29 19:53:36 -0300

    feat: dealing with global vars

--- non_sync/util.py
+++ non_sync/util.py
@@ -2,20 +2,29 @@ from contextlib import contextmanager
 from datetime import datetime
 import time
 
+GLOBAL = [1,]
 
 def long_func(number: int):
     print(f'[long_process:{number}] --------- START ---------')
+
+    GLOBAL.append(GLOBAL[-1] + 1)
+    print(f'[long_process:{number}] GLOBAL:{GLOBAL}')
+
     time.sleep(3)
     print(f'[long_process:{number}] realized long_process')
 
     print(f'[long_process:{number}] --------- END ---------')
 
 async def async_long_func(number: int):
-    print(f'[long_process:{number}] --------- START ---------')
-    time.sleep(3)
-    print(f'[long_process:{number}] realized long_process')
+    print(f'[async_long_func:{number}] --------- START ---------')
 
-    print(f'[long_process:{number}] --------- END ---------')
+    GLOBAL.append(GLOBAL[-1] + 1)
+    print(f'[async_long_func:{number}] GLOBAL:{GLOBAL}')
+
+    time.sleep(3)
+    print(f'[async_long_func:{number}] realized long_process')
+
+    print(f'[async_long_func:{number}] --------- END ---------')
 
 @contextmanager
 def time_execution():
